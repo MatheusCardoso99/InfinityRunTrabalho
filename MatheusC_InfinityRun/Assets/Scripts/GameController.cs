d3802f3ed879bbd5b03e954b466798a0aa40b213
MatheusCardoso99 <86811388+MatheusCardoso99@users.noreply.github.com>
2023-11-06 22:42:16 -0300

    PrimeiroCommit

---
+++ MatheusC_InfinityRun/Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    //Propriedade do Chao
+    [Header("Configuracao do Chao")]
+    public float        _chaoDestruido;
+    public float        _chaoTamanho;   
+    public float        _chaoVelocidade;
+    public GameObject _chaoPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

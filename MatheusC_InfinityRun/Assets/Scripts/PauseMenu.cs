e523fdc09686eea1600d8cfa4db0a7507653629a
MatheusCardoso99 <86811388+MatheusCardoso99@users.noreply.github.com>
2023-11-11 19:48:51 -0300

    Menu de pause

---
+++ MatheusC_InfinityRun/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public Transform    _pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_pauseMenu.gameObject.activeSelf)
+            {
+                _pauseMenu.gameObject.SetActive(false);
+                Time.timeScale = 1;//Pause o jogo
+            }
+            else
+            {
+                _pauseMenu.gameObject.SetActive(true);
+                Time.timeScale = 0;//Pause o jogo
+            }
+        }                
+    }
+
+    public void ResumeGame()
+    {
+        if (_pauseMenu.gameObject.activeSelf)
+        {
+            _pauseMenu.gameObject.SetActive(false);
+            Time.timeScale = 1;//Pause o jogo
+        }
+        else
+        {
+            _pauseMenu.gameObject.SetActive(true);
+            Time.timeScale = 0;//Pause o jogo
+        }
+    }
+    
+}
+
